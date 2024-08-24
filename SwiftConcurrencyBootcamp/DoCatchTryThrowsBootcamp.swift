af907ef34a3b1236d21586de88c37f147de5c7b2
jackie <ss611611@gmail.com>
2024-08-24 16:23:26 +0800

    How to use Do, Try, Catch, and Throws in Swift

---
+++ SwiftConcurrencyBootcamp/DoCatchTryThrowsBootcamp.swift
@@ -0,0 +1,108 @@
+//
+//  DoCatchTryThrowsBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Jackie Lu on 2024/8/24.
+//
+
+import SwiftUI
+
+class DoCatchTryThrowsBootcampDataManager {
+    
+    let isActive: Bool = true
+    
+    func getTitle() -> (title: String?, error: Error?) {
+        if isActive {
+            return ("NEW TEXT!", nil)
+        } else {
+            return (nil, URLError(.badURL))
+        }
+    }
+    
+    func getTitle2() -> Result<String, Error> {
+        if isActive {
+            return .success("NEW TEXT!")
+        } else {
+            return .failure(URLError(.appTransportSecurityRequiresSecureConnection))
+        }
+    }
+    
+    func getTitle3() throws -> String {
+        if isActive {
+            return "NEW TEXT!"
+        } else {
+            throw URLError(.badServerResponse)
+        }
+    }
+    
+    func getTitle4() throws -> String {
+        if isActive {
+            return "FINAL TEXT!"
+        } else {
+            throw URLError(.badServerResponse)
+        }
+    }
+}
+
+class DoCatchTryThrowsBootcampViewModel: ObservableObject {
+    
+    @Published var text: String = "Starting text."
+    let manager = DoCatchTryThrowsBootcampDataManager()
+    
+    func featchTitle() {
+        /*
+        let returnValue = manager.getTitle()
+        if let newTitle = returnValue.title {
+            self.text = newTitle
+        } else if let error = returnValue.error {
+            self.text = error.localizedDescription
+        }
+         */
+        /*
+        let result = manager.getTitle2()
+        
+        switch result {
+        case .success(let newTitle):
+            self.text = newTitle
+        case .failure(let error):
+            self.text = error.localizedDescription
+        }
+         */
+        
+        
+//        let newTitle = try? manager.getTitle3()
+//        if let newTitle = newTitle {
+//            self.text = newTitle
+//        }
+        
+        do {
+            let newTitle = try? manager.getTitle3()
+            if let newTitle = newTitle {
+                self.text = newTitle
+            }
+            
+            let finalTitle = try manager.getTitle4()
+            self.text = finalTitle
+        } catch {
+            self.text = error.localizedDescription
+        }
+    }
+}
+
+struct DoCatchTryThrowsBootcamp: View {
+    
+    @StateObject private var viewModel = DoCatchTryThrowsBootcampViewModel()
+    
+    var body: some View {
+        Text(viewModel.text)
+            .frame(width: 300, height: 300)
+            .background(Color.blue)
+            .onTapGesture {
+                viewModel.featchTitle()
+            }
+    }
+}
+
+#Preview {
+    DoCatchTryThrowsBootcamp()
+}
