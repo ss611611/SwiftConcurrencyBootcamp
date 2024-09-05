ce247537179e8b041181d64f8cf1135d20676dd5
jackie <ss611611@gmail.com>
2024-09-05 11:10:22 +0800

    How to use Actors and non-isolated

---
+++ SwiftConcurrencyBootcamp/ActorsBootcamp.swift
@@ -0,0 +1,134 @@
+//
+//  ActorsBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Jackie Lu on 2024/8/27.
+//
+
+import SwiftUI
+
+class MyDataManager {
+    static let instance = MyDataManager()
+    private init() { }
+    
+    var data: [String] = []
+    private let lock = DispatchQueue(label: "com.MyApp.MyDataManager")
+    
+    func getRandomData(completionHandler: @escaping (_ title: String?) -> ()) {
+        lock.async {
+            self.data.append(UUID().uuidString)
+            print(Thread())
+            completionHandler(self.data.randomElement())
+        }
+    }
+}
+
+actor MyActorDataManager {
+    static let instance = MyActorDataManager()
+    private init() { }
+    
+    var data: [String] = []
+    
+    func getRandomData() -> String? {
+        self.data.append(UUID().uuidString)
+        print(Thread())
+        return self.data.randomElement()
+    }
+    
+    nonisolated func getSaveData() -> String {
+        return "NEW DATA"
+    }
+}
+
+struct HomeView: View {
+    
+    let manager = MyActorDataManager.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.8).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onAppear(perform: {
+            Task {
+                let newString = await manager.getSaveData()
+            }
+        })
+        .onReceive(timer) { _ in
+            Task {
+                if let data = await manager.getRandomData() {
+                    await MainActor.run {
+                        self.text = data
+                    }
+                }
+            }
+//            DispatchQueue.global(qos: .background).async {
+//                manager.getRandomData { title in
+//                    if let data = title {
+//                        DispatchQueue.main.async {
+//                            self.text = data
+//                        }
+//                    }
+//                }
+//            }
+        }
+    }
+}
+
+struct BrowseView: View {
+    
+    let manager = MyActorDataManager.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.yellow.opacity(0.8).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            Task {
+                if let data = await manager.getRandomData() {
+                    await MainActor.run {
+                        self.text = data
+                    }
+                }
+            }
+//            DispatchQueue.global(qos: .default).async {
+//                manager.getRandomData { title in
+//                    if let data = title {
+//                        DispatchQueue.main.async {
+//                            self.text = data
+//                        }
+//                    }
+//                }
+//            }
+        }
+    }
+}
+
+
+struct ActorsBootcamp: View {
+    var body: some View {
+        TabView{
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            BrowseView()
+                .tabItem {
+                    Label("Browse", systemImage: "magnifyingglass")
+                }
+        }
+    }
+}
+
+#Preview {
+    ActorsBootcamp()
+}
