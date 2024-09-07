e5a56b0479f72965a60e598e2c5b838df523d755
jackie <ss611611@gmail.com>
2024-09-07 17:27:11 +0800

    How to use Searchable, Search Suggestions, Search Scopes - Searchable

---
+++ SwiftConcurrencyBootcamp/SearchableBootcamp.swift
@@ -0,0 +1,85 @@
+ //
+//  SearchableBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Jackie Lu on 2024/9/7.
+//
+
+import SwiftUI
+
+struct Restaurant: Identifiable, Hashable {
+    let id: String
+    let title: String
+    let cuisine: CuisineOption
+}
+
+enum CuisineOption: String {
+    case american, italian, japanese
+}
+
+final class RestaurantManager {
+    
+    func getAllRestaurants() async throws -> [Restaurant] {
+        [
+            Restaurant(id: "1", title: "Burger Shack", cuisine: .american),
+            Restaurant(id: "2", title: "Pasta Palace", cuisine: .italian),
+            Restaurant(id: "3", title: "Sushi Heaven", cuisine: .japanese),
+            Restaurant(id: "4", title: "Local Market", cuisine: .american)
+        ]
+    }
+}
+
+@MainActor
+final class SearchableViewModel: ObservableObject {
+    
+    @Published private(set) var allRestaurants: [Restaurant] = []
+    @Published var searchText: String = ""
+    let manager = RestaurantManager()
+    
+    func loadestaurants() async {
+        do {
+            allRestaurants = try await manager.getAllRestaurants()
+        } catch {
+            print(error)
+        }
+    }
+}
+
+struct SearchableBootcamp: View {
+    
+    @StateObject private var viewModel = SearchableViewModel()
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 20) {
+                ForEach(viewModel.allRestaurants) { restaurant in
+                    RestaurantRow(restaurant: restaurant)
+                }
+            }
+            .padding()
+        }
+        .searchable(text: $viewModel.searchText, placement: .automatic, prompt: Text("Search restaurants..."))
+//        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("Restaurants")
+        .task {
+            await viewModel.loadestaurants()
+        }
+    }
+    
+    private func RestaurantRow(restaurant: Restaurant) -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(restaurant.title)
+                .font(.headline)
+            Text(restaurant.cuisine.rawValue.capitalized)
+                .font(.caption)
+        }
+        .padding()
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+        .background(Color.black.opacity(0.05))
+    }
+}
+
+#Preview {
+    NavigationStack {
+        SearchableBootcamp()
+    }
+}
